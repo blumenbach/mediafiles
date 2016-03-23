b2da6ad11dd762341ac055fab816c9e26061cc83
Christopher Johnson <christopher.johnson@wikimedia.de>
2016-03-23 13:41:41 +0100

    adds basic auth

--- app.js
+++ app.js
@@ -4,18 +4,36 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var basicAuth = require('basic-auth');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+var auth = function (req, res, next) {
+    function unauthorized(res) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        return res.send(401);
+    }
+
+    var user = basicAuth(req);
+
+    if (!user || !user.name || !user.pass) {
+        return unauthorized(res);
+    }
+
+    if (user.name === 'foo' && user.pass === 'bar') {
+        return next();
+    } else {
+        return unauthorized(res);
+    }
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,12 +49,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //    return res.render('media', { galleryHtml : req.html });
 //});
 app.use('/users', users);
-app.use('/media', require('./lib/gallery.js')({
+app.use('/', require('./lib/gallery.js')({
     staticFiles : 'resources/media',
-    urlRoot : 'media',
+    urlRoot : '/',
     title : 'Blumenbach Sammlung',
     render : false
-}), function(req, res, next){
+}), auth, function(req, res, next){
   return res.render('media', { galleryHtml : req.html });
 });
 
